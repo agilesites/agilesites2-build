3993e532c71ff3e5a6b32e83ca442df4f4fa0144
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-09-04 11:01:11 +0200

    working attributes - constant values

--- src/main/scala/agilesitesng/deploy/actor/Decoder.scala
+++ src/main/scala/agilesitesng/deploy/actor/Decoder.scala
@@ -2,7 +2,7 @@ package agilesitesng.deploy.actor
 
 import agilesitesng.Utils
 import agilesitesng.deploy.model.SpoonModel
-import agilesitesng.deploy.model.SpoonModel.{AttributeEditor, Site}
+import agilesitesng.deploy.model.SpoonModel.{Attribute, AttributeEditor, Site}
 
 /**
  * Created by msciab on 20/08/15.
@@ -12,27 +12,44 @@ class Decoder(site: String, username: String, password: String) extends Utils {
   /**
    * @return a map ready to be deployed
    */
-  def map(op: String, t2: Tuple2[String, String]*) = t2.toMap +
-    ("op" -> op) +
-    ("site" -> site) +
-    ("username" -> username) +
-    ("password" -> password)
+  def deploy(value: String, id: Long, name: String, description: String, t2: Tuple2[Symbol, String]*) =
+    t2.map(x => x._1.name -> x._2).toMap +
+      ("op" -> "deploy") +
+      ("value" -> value) +
+      ("cid" -> id.toString) +
+      ("name" -> name) +
+      ("descript" -> description) +
+      ("site" -> site) +
+      ("username" -> username) +
+      ("password" -> password)
 
   def apply(model: SpoonModel): Map[String, String] = model match {
-    case Site(id, name) => map("site",
+
+    //TODO add description here
+    case AttributeEditor(id, name, file) =>
+      deploy("AttributeEditor", id, name, name,
+        'cid -> id.toString,
+        'xml -> readFile(file))
+
+    // TODO mul cab be Single, Multiple and ORDERED - need another field
+    case Attribute(id, name, description, mul, attributeType, editor, assetType, subtypes) =>
+      deploy("Attribute", id, name, description,
+        'type -> "STRING",
+        'c -> "PageAttribute",
+        'mul -> "S",
+        'existDep -> "false",
+        'notEmbedded -> "false",
+        'attributetype -> "0", //not defined
+        'assettypename -> "Page",
+        'assetsubtypename -> "" // a|b|c
+      )
+
+    case Site(id, name) => Map(
+      "op" -> "site",
       "id" -> id.toString,
       "name" -> name)
 
-    case AttributeEditor(id, name, file) => map("deploy",
-      "id" -> id.toString,
-      "name" -> name,
-      "description" -> name,
-      "value" -> "AttributeEditor",
-      "debug" -> "yes",
-      "cid" -> id.toString,
-      "xml" -> readFile(file))
-
-    case x => map("echo",
+    case x => Map("op" -> "echo",
       "value" -> s"${x.getClass} not recognized")
   }
 
